973d863b635cc74d633973a1401b11bbc0740b19
Raquel Luzes <raqueluzes@gmail.com>
2023-05-30 15:14:57 +0200

    fuun

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get '/tasks', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get '/tasks', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
